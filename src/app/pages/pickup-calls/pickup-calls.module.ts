a8475eaa9ad9826247d0f2def4ebc10916696149
themordhon <romdhonm12@gmail.com>
2022-02-12 10:29:59 +0700

    create pickup-calls page

--- src/app/pages/pickup-calls/pickup-calls.module.ts
+++ src/app/pages/pickup-calls/pickup-calls.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { PickupCallsPageRoutingModule } from './pickup-calls-routing.module';
 
-import { FolderPage } from './folder.page';
+import { PickupCallsPage } from './pickup-calls.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    PickupCallsPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [PickupCallsPage]
 })
-export class FolderPageModule {}
+export class PickupCallsPageModule {}
